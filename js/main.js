2a11878339e67f57d5431b43f5ea058186dd990a
Prashant Gupta <prashantgupta@us.ibm.com>
2017-04-02 16:58:37 -0700

    initial commit for SPA

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var myApp = angular.module('myApp', []);
+var module = angular.module("myApp", ["ngRoute"]);
+
+module.config(function($routeProvider){
+  $routeProvider
+    .when("/", {
+      templateUrl : "pages/home.html"
+    })
+    .when("/founders", {
+      templateUrl : "pages/founders.html"
+    })
+});
+//module.controller("Mainctrl", test);
 //
 // function FooterCtrl($scope) {
 //     $scope.header = {name: "footer.html", url: "footer.html"};
---
+++ old code/pages/js/main.js
@@ -0,0 +1,7 @@
+var module = angular.module("myApp", []);
+
+//module.controller("Mainctrl", test);
+//
+// function FooterCtrl($scope) {
+//     $scope.header = {name: "footer.html", url: "footer.html"};
+// }
