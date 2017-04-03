66969743f9ddadd0e2b2419542b11710a9654a08
Prashant Gupta <prashantgupta@us.ibm.com>
2017-04-02 18:34:28 -0700

    working with angular

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
-var module = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("myApp", ["ngRoute"]);
 
-module.config(function($routeProvider){
+app.controller("mainCtrl", mainFunction);
+
+function mainFunction($scope) {
+  $scope.message = "main";
+  $scope.allPages = [
+    {url: "#/", name: "Home"},
+    {url: "#founders", name: "founders"}
+  ];
+
+  $scope.idSelectedPage=0;
+
+  $scope.setSelected = function(id) {
+    console.log("Received id" + id);
+    $scope.idSelectedPage=id;
+  }
+}
+
+app.controller("foundersCtrl", foundersFunction);
+
+function foundersFunction($scope) {
+  $scope.message = "founders";
+  $scope.idSelectedPage=1;
+}
+
+app.config(function($routeProvider){
   $routeProvider
     .when("/", {
-      templateUrl : "pages/home.html"
+      templateUrl : "pages/home.html",
+      controller  : "mainCtrl"
     })
     .when("/founders", {
-      templateUrl : "pages/founders.html"
+      templateUrl : "pages/founders.html",
+      controller  : "mainCtrl"
     })
 });
-//module.controller("Mainctrl", test);
+
+//app.controller("Mainctrl", test);
 //
 // function FooterCtrl($scope) {
 //     $scope.header = {name: "footer.html", url: "footer.html"};
